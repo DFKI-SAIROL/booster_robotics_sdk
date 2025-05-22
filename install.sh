d7e26b53477dd0d91d7fea3f3af1ee9da72f4e7d
Puze Liu <puze@robot-learning.de>
2025-05-22 12:41:57 +0200

    Update  make to avoid systemwide installation

--- install.sh
+++ install.sh
@@ -14,14 +14,14 @@ echo "Third Party Dir = $third_party_dir"
 
 set -e
 
-apt update
+# apt update
 
-apt install git
-apt install build-essential
-apt install cmake
-apt install libssl-dev
-apt install libasio-dev
-apt install libtinyxml2-dev
+# apt install git
+# apt install build-essential
+# apt install cmake
+# apt install libssl-dev
+# apt install libasio-dev
+# apt install libtinyxml2-dev
 
 ubuntu_version=$(lsb_release -rs)
 ubuntu_version_flag=20
@@ -43,12 +43,30 @@ fi
 echo "SDK Lib Dir = $booster_sdk_lib_dir"
 echo "Third Party Lib Dir = $third_party_lib_dir"
 
-cp -r $booster_sdk_dir/include/* /usr/local/include
-cp -r $booster_sdk_lib_dir/* /usr/local/lib
+install_dir=$(echo $HOME)/library/booster
+# install_dir=/usr/local
+echo "Install Dir = $install_dir"
+
+mkdir -p $install_dir/include
+mkdir -p $install_dir/lib
+
+cp -r $booster_sdk_dir/include/* $install_dir/include
+cp -r $booster_sdk_lib_dir/* $install_dir/lib
 echo "Booster Robotics SDK installed successfully!"
 
-cp -r $third_party_dir/include/* /usr/local/include
-cp -r $third_party_lib_dir/* /usr/local/lib
+cp -r $third_party_dir/include/* $install_dir/include
+cp -r $third_party_lib_dir/* $install_dir/lib
 echo "Third Party Libraries installed successfully!"
 
-ldconfig
+# ldconfig
+
+cd $install_dir/lib
+cd $install_dir/lib
+for lib in *.so; do
+    [ -f "$lib" ] || continue
+    soname=$(readelf -d "$lib" | grep SONAME | sed -E 's/.*\[(.*)\].*/\1/')
+    if [ -n "$soname" ] && [ "$soname" != "$lib" ] && [ ! -e "$soname" ]; then
+        ln -sf "$lib" "$soname"
+        echo "Created symlink: $soname -> $lib"
+    fi
+done
